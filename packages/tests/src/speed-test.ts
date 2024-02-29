110b1f869dd5d3cb1a2c6f6c46ec8462ed3c4d91
Vaibhav Muchandi <vaibhavmm@hotmail.com>
2024-02-29 20:35:06 +0530

    modify: tests based on events

--- packages/tests/src/speed-test.ts
+++ packages/tests/src/speed-test.ts
@@ -1,11 +1,31 @@
 import { DSTPClient, createLibp2pNode } from "client"
 import { multiaddr } from "@multiformats/multiaddr";
 
-const nodeAddr = multiaddr("/ip4/122.167.191.234/tcp/44444/ws/p2p/12D3KooWKfRkhci6oPf43AKpkHGoCM38mtbAfc6UyKv2rgp8NzSN")
+const nodeAddr = multiaddr("/ip4/122.167.190.28/tcp/44444/ws/p2p/12D3KooWSdggeL27rJWQgiMM62PCjcPDJG3HboZuERphTsQ2SbEp")
 
 async function main() {
     const node = await createLibp2pNode()
     const dstp = DSTPClient.init(node)
+
+    dstp.on("download-speed", (data) => {
+        console.log('Current download speed:', data.speedMbps)
+    })
+
+    dstp.on("upload-speed", (data) => {
+        console.log('Current upload speed:', data.speedMbps)
+    })
+
+    dstp.on("download-speed-final", (data) => {
+        console.log('Final download speed: ', data.speedMbps)
+    })
+
+    dstp.on("upload-speed-final", (data) => {
+        console.log('Upload speed final: ', data.speedMbps)
+    })
+
+    dstp.on("ping", (data) => {
+        console.log('Ping: ', data.ping)
+    })
 
     await dstp.uploadTest(nodeAddr)
     await dstp.downloadTest(nodeAddr)
